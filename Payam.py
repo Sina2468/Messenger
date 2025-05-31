bfeaede71aeeff9d33c2fa59d48912e0051a3db8
Sina2468 <203882460+Sina2468@users.noreply.github.com>
2025-05-31 18:41:34 +0330

    creat base from of Project

---
+++ Payam.py
@@ -0,0 +1,5 @@
+from Starter import Starter
+
+
+if __name__ == "__main__":
+    Starter()
---
+++ Starter.py
@@ -0,0 +1,6 @@
+import Setting
+
+
+def Starter():
+    print(Setting.Menu)
+    Setting.Dist_IP = str(input("\nDist-IP >>> "))
